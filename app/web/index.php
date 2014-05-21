ac79f0616d925cd4fce55da53e8506306b3c4824
Zaski <zaski123@gmail.com>
2014-05-21 20:33:01 +0200

    Cleaned basic directory structure of the Brain server

--- app/web/index.php
+++ app/web/index.php
@@ -2,15 +2,20 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-/*
+
 //Neo4j client initializacion
 $neo4jclient = new Everyman\Neo4j\Client('localhost', 7474);
 
-print_r($client->getServerInfo());
-*/
+//print_r($neo4jclient->getServerInfo());
 
 //Silex app initialization
 $app = new Silex\Application();
+//Activate debug mode (show errors)
+$app['debug'] = true;
+
+$app->get('/', function() use($app){
+    return "Works!";
+});
  
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
