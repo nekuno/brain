483d4c3edff0e95f699e5364efd15b9f38820f57
Adrián Ríos <adrian.web.dev@gmail.com>
2014-06-06 12:48:48 +0200

    add POST action for users resource

--- app/src/controllers.php
+++ app/src/controllers.php
@@ -2,19 +2,14 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get(
-    '/{name}',
-    function ($name) use ($app) {
+    '/',
+    function () use ($app) {
 
-        return $app['twig']->render('index.html', array(
-          'name' => $name,
-        ));
+        return $app['twig']->render('index.html');
     }
 )
     ->bind('homepage');
@@ -24,7 +19,7 @@ $app->get(
     function () use ($app) {
         $client = $app['neo4j.client'];
         $error = array();
-        for($i = 1; $i <= 1; $i++){
+        for ($i = 1; $i <= 1; $i++) {
             $username = 'username' . $i;
             $query = new Everyman\Neo4j\Cypher\Query(
                 $client,
@@ -33,20 +28,19 @@ $app->get(
 
             try {
                 $client->executeCypherQuery($query);
-            } catch(\Exception $e) {
+            } catch (\Exception $e) {
                 $error[] = sprintf('Error al añadir el usuario %s', $i);
             }
         }
 
-        if(!empty($error)){
+        if (!empty($error)) {
             return var_dump($error);
         }
 
         return 'Complete!';
     }
 )
-->bind('load_users');
-
+    ->bind('load_users');
 
 $app->get(
     '/users/remove',
@@ -61,11 +55,11 @@ $app->get(
 
         try {
             $client->executeCypherQuery($query);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             $error[] = $e->getMessage();
         }
 
-        if(!empty($error)){
+        if (!empty($error)) {
             return var_dump($error);
         }
 
@@ -94,7 +88,50 @@ $app->get(
 )
     ->bind('view_users');
 
+/**
+ * Adds new User.
+ */
+$app->post(
+    '/users',
+    function (Request $request) use ($app) {
 
+        $neo4j = $app['neo4j.client'];
+
+        // Parse request content and populate parameters
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+
+        $query = new Everyman\Neo4j\Cypher\Query(
+            $neo4j,
+            "CREATE (u:User { status: 1,
+                qnoow_id: " . $request->request->get('id') . ",
+                username: '" . $request->request->get('username') . "',
+                email: '" . $request->request->get('email') . "'
+            }) RETURN u;"
+        );
+
+        // Execute query and get new created user
+        $result = $query->getResultSet();
+
+        $user = array();
+
+        foreach ($result as $row) {
+            $user['username'] = $row['u']->getProperty('username');
+            $user['email'] = $row['u']->getProperty('email');
+            $user['qnoow_id'] = $row['u']->getProperty('qnoow_id');
+        }
+
+        if (!empty($user)) {
+            $code = 201;
+
+        } else {
+            $code = 200;
+        }
+
+        return $app->json($user, $code);
+
+    }
+)->bind('users-add');
 
 $app->error(
     function (\Exception $e, $code) use ($app) {
@@ -112,4 +149,5 @@ $app->error(
 
         return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
     }
+
 );
