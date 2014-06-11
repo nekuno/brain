408df5e3e0eee876a48e52e92b9aa00238b1436b
Zaski <zaski123@gmail.com>
2014-06-11 21:22:08 +0200

    Created QuestionModel. Decoupled Query creation (now in the model) from the controller.

--- app/src/controller/questions.php
+++ app/src/controller/questions.php
@@ -6,44 +6,13 @@ use Symfony\Component\HttpFoundation\Request;
 $app->post(
     '/questions',
     function(Request $request) use($app) {
-        $neo4j = $app['neo4j.client'];
 
-        //Parse request content
-        $data = json_decode($request -> getContent(), true);
-        $request->request->replace(is_array($data) ? $data : array());
+        $client = $app['neo4j.client'];
+        $model = new \model\QuestionModel($client);
+        $data = $request->request->all();
+        $response = $model->create($data);
 
-        //Construct the query string
-        $query = "CREATE (q:QUESTION {_id: '" . $request->request->get("id") . "', _content: '". $request->request->get("content") . "'}), ";
-
-        $answers = $request->request->get("answers");
-        $numAnswers = count($answers);
-        $count = 0;
-        foreach($answers as $answer){
-            $query .= "(:ANSWER {_id: '" . $answer['id'] . "', _content: '" . $answer['content'] . "'})-[:IS_ANSWER_OF]->(q)";
-            if(++$count === $numAnswers){
-                $query .= ";";
-            }
-            else{
-                $query .= ", ";
-            }
-        }
-
-        //Create the Neo4j query object
-        $neoQuery = new Everyman\Neo4j\Cypher\Query(
-            $neo4j,
-            $query
-        );
-
-        //Execute query and get new created user
-        try{
-            $result = $neoQuery->getResultSet();
-        } catch(\Exception $e){
-            return $app->json(array(), 400);
-        }
-        //TODO: implement a decent result
-
-
-        return "Question with answers created correctly";
+        return $app->json($response, !empty($response) ? 201 : 200);
 
     }
 )->bind('add_question');
---
+++ app/src/model/QuestionModel.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zaski
+ * Date: 6/11/14
+ * Time: 8:57 PM
+ */
+
+namespace model;
+
+use Everyman\Neo4j\Client;
+use Everyman\Neo4j\Cypher\Query;
+
+class QuestionModel {
+
+    protected $client;
+
+    public function __construct(Client $client){
+        $this->client = $client;
+    }
+
+    public function create(array $data = array() ){
+
+        //Construct the query string
+        $query =
+            "CREATE
+                (q:Qestion {
+                    _id: '" . $data['id'] . "',
+                    _text: '". $data['text'] . "'}), ";
+
+        $answers =$data['answers'];
+        $numAnswers = count($answers);
+        $count = 0;
+        foreach($answers as $answer){
+            $query .=
+                "(:Answer {
+                    _id: '" . $answer['id'] . "',
+                    _text: '" . $answer['text'] . "'
+                })-[:IS_ANSWER_OF]->(q)";
+            if(++$count === $numAnswers){
+                $query .= ";";
+            }
+            else{
+                $query .= ", ";
+            }
+        }
+
+        //Create the Neo4j query object
+        $neoQuery = new  Query (
+            $this->client,
+            $query
+        );
+
+        //Execute query and get new created question
+        $result = $neoQuery->getResultSet();
+
+        //TECHNICALDEBT: implement a decent result
+
+        $response = array();
+        $response['creation'] = "ok";
+
+        return $response;
+    }
+
+} 
