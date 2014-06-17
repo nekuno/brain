f1b4632a0dc717ca710471bc438cb2cf37fddcbc
adridev <adrian.web.dev@gmail.com>
2014-06-17 11:12:48 +0200

    reorder routes to fix route-matching

--- app/src/routing.php
+++ app/src/routing.php
@@ -1,11 +1,11 @@
 <?php
 
 // User routes
+$app->get('/users', 'users.controller:indexAction');
+$app->post('/users', 'users.controller:addAction');
 $app->get('/users/matching', 'users.controller:getMatchingAction')->value('id1', null)->value('id2', null);
 $app->get('/users/{id}', 'users.controller:showAction')->value('id', null);
 $app->delete('/users/{id}', 'users.controller:deleteAction')->value('id', null);
-$app->get('/users', 'users.controller:indexAction');
-$app->post('/users', 'users.controller:addAction');
 
 // Question routes
 $app->post('/questions/answers', 'questions.controller:answerAction');
