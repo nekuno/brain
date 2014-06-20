c85460cdc3e51f0d9552817b893991a26bee8b89
adridev <adrian.web.dev@gmail.com>
2014-06-20 18:52:53 +0200

    add doctrine DBAL to project

--- src/app.php
+++ src/app.php
@@ -3,6 +3,7 @@
 use Guzzle\GuzzleServiceProvider;
 use Provider\Neo4jPHPServiceProvider;
 use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
@@ -13,6 +14,16 @@ $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
+$app->register(new DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'    => 'pdo_mysql',
+        'host'      => '127.0.0.1',
+        'dbname'    => 'qnoow',
+        'user'      => 'qnoowdev',
+        'password'  => 'qnoow2014',
+        'charset'   => 'utf8',
+    ),
+));
 $app->register(new Neo4jPHPServiceProvider());
 $app->register(new GuzzleServiceProvider());
 
