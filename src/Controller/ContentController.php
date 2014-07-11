1db1ef27680a61ee51bb32a7bee8b5a9573ba089
adridev <adrian.web.dev@gmail.com>
2014-07-11 15:32:26 +0200

    refactor Scraper class

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -4,24 +4,36 @@ namespace Controller;
 
 use ApiConsumer\Auth\DBUserProvider;
 use ApiConsumer\Restful\Consumer\ConsumerFactory;
+use ApiConsumer\Scraper\Metadata;
 use ApiConsumer\Scraper\Scraper;
 use ApiConsumer\Storage\DBStorage;
 use Goutte\Client;
-use Model\ContentModel;
+use Model\LinkModel;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class ContentController
+ *
+ * @package Controller
+ */
 class ContentController
 {
 
+    /**
+     * @param Request $request
+     * @param Application $app
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @throws \Exception
+     */
     public function addLink(Request $request, Application $app)
     {
 
         $data = $request->request->all();
 
         try {
-            /** @var ContentModel $model */
-            $model  = $app['content.model'];
+            /** @var LinkModel $model */
+            $model  = $app['links.model'];
             $result = $model->addLink($data);
         } catch (\Exception $e) {
             if ($app['env'] == 'dev') {
@@ -32,9 +44,14 @@ class ContentController
         }
 
         return $app->json($result, empty($result) ? 200 : 201);
-
     }
 
+    /**
+     * @param Request $request
+     * @param Application $app
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @throws \Exception
+     */
     public function fetchLinksAction(Request $request, Application $app)
     {
 
@@ -45,7 +62,111 @@ class ContentController
             return $app->json(array(), 400);
         }
 
-        $storage      = new DBStorage($app['content.model']);
+        $storage = new DBStorage($app['links.model']);
+
+        $consumer = $this->getConsumer($app, $resource);
+
+        try {
+            $linksGroupedByUser = $consumer->fetchLinks($userId);
+
+            $processedLinks = $this->processLinks($linksGroupedByUser);
+
+            $storage->storeLinks($processedLinks);
+
+            $errors = $storage->getErrors();
+            if (array() !== $errors) {
+                foreach ($errors as $error) {
+                    $app->json($errors, 500); // TODO: this is only for development
+                    $app['monolog']->addDebug($error);
+                }
+            }
+        } catch (\Exception $e) {
+            return $app->json($this->getError($e), 500);
+        }
+
+        return $app->json($processedLinks);
+    }
+
+    /**
+     * @param $linksGroupedByUser
+     * @return array
+     */
+    private function processLinks(array $linksGroupedByUser = array())
+    {
+
+        $processedLinks = array();
+
+        foreach ($linksGroupedByUser as $user => $userLinks) {
+
+            $userProcessedLinks = $this->processUserLinks($userLinks);
+
+            $processedLinks[$user] = $userProcessedLinks;
+        }
+
+        return $processedLinks;
+    }
+
+    /**
+     * @param $userLinks
+     * @return array
+     */
+    private function processUserLinks($userLinks)
+    {
+
+        $processedLinks = array();
+
+        foreach ($userLinks as $link) {
+
+            $metadata = $this->getMetadata($link['url']);
+
+            $metaTags = $metadata->getMetaTags();
+
+            $metaOgData = $metadata->extractOgMetadata($metaTags);
+
+            if (array() !== $metaOgData) {
+                $link = $metadata->mergeLinkMetadata($metaOgData, $link);
+            } else {
+                $metaDefaultData = $metadata->extractDefaultMetadata($metaTags);
+                if (array() !== $metaDefaultData) {
+                    $link = $metadata->mergeLinkMetadata($metaDefaultData, $link);
+                }
+            }
+
+            $processedLinks[] = $link;
+        }
+
+        return $processedLinks;
+    }
+
+    /**
+     * @param $url
+     * @throws \Exception
+     * @return \ApiConsumer\Scraper\Metadata
+     */
+    private function getMetadata($url)
+    {
+
+        $goutte = new Client();
+
+        try {
+            $scraper = new Scraper($goutte);
+            $crawler = $scraper->initCrawler($url)->scrap('//meta | //title');
+
+            return new Metadata($crawler);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @param Application $app
+     * @param $resource
+     * @return \ApiConsumer\Restful\Consumer\LinksConsumerInterface
+     * @throws \Exception
+     */
+    private function getConsumer(Application $app, $resource)
+    {
+
         $userProvider = new DBUserProvider($app['db']);
         $httpClient   = $app['guzzle.client'];
 
@@ -58,105 +179,7 @@ class ContentController
             );
         }
 
-        $consumer = ConsumerFactory::create($resource, $userProvider, $httpClient, $options);
-
-        try {
-
-            $linksGroupedByUser = $consumer->fetchLinks($userId);
-
-            $processedLinks = array();
-
-            foreach ($linksGroupedByUser as $user => $userLinks) {
-                $processedLinks[$user] = $this->scrapLinksMetadata($userLinks);
-            }
-
-            $storage->storeLinks($processedLinks);
-
-            $errors = $storage->getErrors();
-
-            if (array() !== $errors) {
-                foreach ($errors as $error) {
-                    $app->json($errors, 500); // TODO: this is only for development
-                    $app['monolog']->addDebug($error);
-                }
-
-            }
-
-        } catch (\Exception $e) {
-            return $app->json($this->getError($e), 500);
-        }
-
-        return $app->json($processedLinks);
-
-    }
-
-    private function scrapLinksMetadata(array $links = array())
-    {
-
-        $processedLinks = array();
-
-        foreach ($links as $link) {
-
-            $metadata = $this->getMetadata($link);
-
-            $metaTags = $metadata->getMetaTags();
-
-            $metaOgData = $metadata->extractOgMetadata($metaTags);
-            if (array() !== $metaOgData) {
-                $link = $this->mergeLinkMetadata($metaOgData, $link);
-            } else {
-                $metaDefaultData = $metadata->extractDefaultMetadata($metaTags);
-                if (array() !== $metaDefaultData) {
-                    $link = $this->mergeLinkMetadata($metaDefaultData, $link);
-                }
-            }
-
-            $processedLinks[] = $link;
-
-        }
-
-        return $processedLinks;
-
-    }
-
-    /**
-     * @param $link
-     * @return \ApiConsumer\Scraper\Metadata
-     */
-    private function getMetadata($link)
-    {
-
-        $scraper = new Scraper(new Client(), $link['url']);
-
-        $metadata = $scraper->scrap();
-
-        return $metadata;
-    }
-
-    /**
-     * @param $scrapedData
-     * @param $link
-     * @return mixed
-     */
-    private function mergeLinkMetadata(array $scrapedData, array $link)
-    {
-
-        foreach ($scrapedData as $meta) {
-            if (array_key_exists('title', $meta) && null !== $meta['title']) {
-                $link['title'] = $meta['title'];
-            }
-
-            if (array_key_exists('description', $meta) && null !== $meta['description']) {
-                $link['description'] = $meta['description'];
-            }
-
-            if (array_key_exists('canonical', $meta) && null !== $meta['canonical']) {
-                $link['url'] = $meta['canonical'];
-            }
-        }
-
-        return $link;
-
+        return ConsumerFactory::create($resource, $userProvider, $httpClient, $options);
     }
 
     /**
