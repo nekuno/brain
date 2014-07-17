631253bf4616221d59c0139ecd81e3fcd5bcaeb3
adridev <adrian.web.dev@gmail.com>
2014-07-17 13:27:54 +0200

    change default entity manager name

--- config/cli-config.php
+++ config/cli-config.php
@@ -4,6 +4,6 @@ use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 // replace with mechanism to retrieve EntityManager in your app
 $app = require __DIR__.'/../src/app.php';
-$entityManager = $app['orm.ems']['brain'];
+$entityManager = $app['orm.ems']['mysql_brain'];
 
 return ConsoleRunner::createHelperSet($entityManager);
