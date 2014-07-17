649581734df5e15df5942ae19c3f1ee1b6b1400c
adridev <adrian.web.dev@gmail.com>
2014-07-17 03:32:08 +0200

    fix commands configuration

--- config/cli-config.php
+++ config/cli-config.php
@@ -3,6 +3,7 @@
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 // replace with mechanism to retrieve EntityManager in your app
+$app = require __DIR__.'/../src/app.php';
 $entityManager = $app['orm.ems']['brain'];
 
 return ConsoleRunner::createHelperSet($entityManager);
