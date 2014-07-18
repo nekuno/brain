f521b52df4605f22514000e52feddf8e3fa5fe75
adridev <adrian.web.dev@gmail.com>
2014-07-18 18:33:17 +0200

    rename class

--- src/Console/Command/FetchLinksCommand.php
+++ src/Console/Command/FetchLinksCommand.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FetchLinksCommand extends ContainerAwareCommand
+class FetchLinksCommand extends ApplicationAwareCommand
 {
 
     protected function configure()
