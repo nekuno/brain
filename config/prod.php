0ff9996b18a73cd2a413d1df7e990c6276e2bf25
adridev <adrian.web.dev@gmail.com>
2014-07-24 18:36:07 +0200

    configure monolog on production env

--- config/prod.php
+++ config/prod.php
@@ -1,5 +1,14 @@
 <?php
 
 // configure your app for the production environment
+use Silex\Provider\MonologServiceProvider;
+
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+// enable the debug mode
+$app['debug'] = true;
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/silex_prod.log',
+));
