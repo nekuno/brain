bd2296aae707478affb56fc1ad477c91f8a3b785
adridev <adrian.web.dev@gmail.com>
2014-07-24 18:37:40 +0200

    clean dead code

--- web/index.php
+++ web/index.php
@@ -6,8 +6,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../src/app.php';
 
-$app['env'] = 'prod';
-
 require __DIR__.'/../config/prod.php';
 require __DIR__.'/../src/controllers.php';
 require __DIR__.'/../src/models.php';
