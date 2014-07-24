0d34ee36eaca866aa2bfcc19f35a77f44f5cbf23
Sergio Fernández <sergio@yoprogramador.com>
2014-07-24 18:02:53 +0200

    Fix change create links query

--- src/Model/LinkModel.php
+++ src/Model/LinkModel.php
@@ -42,13 +42,13 @@ class LinkModel
                 "WHERE u.qnoow_id = {userId}"
                 . " CREATE "
                 . " (l:Link {url: {url}, title: {title}, description: {description}, processed: 0})"
-                . ", (l)<-[r:LIKES]-(u) "
+                . ", (u)-[r:LIKES]->(l) "
                 . " RETURN l;";
         } else {
             $template = "MATCH (u:User)"
                 . ", (l:Link) "
                 . " WHERE u.qnoow_id = {userId} AND l.url = {url}"
-                . " CREATE UNIQUE (l)<-[r:LIKES]-(u)"
+                . " CREATE UNIQUE (u)-[r:LIKES]->(l)"
                 . " RETURN l;
             ";
         }
@@ -83,7 +83,7 @@ class LinkModel
 
         $stringQuery = "
             MATCH
-                (l:Link)<-[r:LIKES]-(u)
+                (u:User)-[:LIKES]->(l:Link)
             WHERE l.url = {url}
             RETURN l, u
             LIMIT 1";
