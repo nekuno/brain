05d6dd6f6d15daaa9550eeeee162af19a2ee61dc
Sergio Fernández <sergio@yoprogramador.com>
2014-07-31 18:39:49 +0200

    Add console command for create neo4j constraints

---
+++ src/Model/Neo4j/Constraints.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Model\Neo4j;
+
+use Everyman\Neo4j\Client;
+use Everyman\Neo4j\Cypher\Query;
+use Exception\QueryErrorException;
+
+/**
+ * Class Constraints
+ *
+ * @package Model\Neo4j
+ */
+class Constraints
+{
+
+    /**
+     * @var \Everyman\Neo4j\Client
+     */
+    protected $client;
+
+    /**
+     * @param \Everyman\Neo4j\Client $client
+     */
+    public function __construct(Client $client)
+    {
+
+        $this->client = $client;
+    }
+
+    /**
+     * Load the constraints
+     *
+     * This is mean to be executed with a clear db, ony once.
+     *
+     * @throws \Exception
+     */
+    public function load()
+    {
+        $constraints = array();
+        $constraints[] = "CREATE CONSTRAINT ON (u:User) ASSERT u.qnoow_id IS UNIQUE;";
+        $constraints[] = "CREATE CONSTRAINT ON (q:Question) ASSERT q.qnoow_id IS UNIQUE;";
+        $constraints[] = "CREATE CONSTRAINT ON (a:Answer) ASSERT a.qnoow_id IS UNIQUE;";
+        
+        foreach ($constraints as $query) {
+            $neo4jQuery = new Query(
+                $this->client,
+                $query
+            );
+
+            try {
+                $result = $neo4jQuery->getResultSet();
+            } catch (\Exception $e) {
+                throw $e;
+
+                return;
+            }
+        }
+    }
+}
