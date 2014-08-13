b4ceef32661dce6e0fd958b208bcf70b8b773c24
adridev <adrian.web.dev@gmail.com>
2014-08-13 18:32:55 +0200

    QS-156 add binding, ackknowlegment and persistence if server or worker fails

--- src/Console/Command/FetchLinksQueueCommand.php
+++ src/Console/Command/FetchLinksQueueCommand.php
@@ -5,6 +5,7 @@ namespace Console\Command;
 use ApiConsumer\Auth\DBUserProvider;
 use ApiConsumer\Fetcher\FetcherService;
 use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 use Silex\Application;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,41 +17,58 @@ class FetchLinksQueueCommand extends ApplicationAwareCommand
     {
 
         $this->setName('fetch:links:queue')
-             ->setDescription("Process fetch-links queue")
-             ->setDefinition(
-             array()
+            ->setDescription("Process fetch-links queue")
+            ->setDefinition(
+                array()
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
+        $queueName = 'fetch';
+        $exchange = 'social';
+
         /** @var AMQPConnection $amqp */
         $amqp = $this->app['amqp'];
         $channel = $amqp->channel();
 
-        $channel->queue_declare('fetch-links', false, true, false, false);
+        $channel->queue_declare($queueName, false, true, false, false);
 
-        $channel->basic_consume('fetch-links', '', false, true, false, false, array($this, 'callback'));
+        $channel->queue_bind($queueName, $exchange);
 
-        while(count($channel->callbacks)) {
+        $output->writeln('[*] Waiting for messages. To exit press CTRL+C');
+
+        $channel->basic_qos(null, 1, null);
+
+        $channel->basic_consume($queueName, '', false, false, false, false, array($this, 'processQueuedMessage'));
+
+        while (count($channel->callbacks)) {
             $channel->wait();
         }
 
-        $output->writeln('Success!');
+        $channel->close();
+        $amqp->close();
+
     }
 
-    public function callback($msg){
-        $message = unserialize($msg->body);
-        $resourceOwner = $message['resourceOwner'];
-        $userId = $message['userId'];
+    public function processQueuedMessage(AMQPMessage $message)
+    {
+        $messageBody = unserialize($message->body);
+        $resourceOwner = $messageBody['resourceOwner'];
+        $userId = $messageBody['userId'];
 
         $userProvider = new DBUserProvider($this->app['dbs']['mysql_social']);
-        $user = $userProvider->getUsersByResource($this->app['api_consumer.config']['fetcher'][$resourceOwner]['resourceOwner'], $userId);
+        $user = $userProvider->getUsersByResource(
+            $this->app['api_consumer.config']['fetcher'][$resourceOwner]['resourceOwner'],
+            $userId
+        );
 
         /** @var FetcherService $fetcher */
         $fetcher = $this->app['api_consumer.fetcher'];
 
         $fetcher->fetch($user['id'], $resourceOwner);
+
+        $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
     }
 }
