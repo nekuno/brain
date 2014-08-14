fead17ad89eeb654c4f2cff7e7085b6ad287f1b4
adridev <adrian.web.dev@gmail.com>
2014-08-14 16:36:22 +0200

    Generalize command extracting fetch logic to an expecific worker

--- src/Console/Command/RabbitMqWorkersUpCommand.php
+++ src/Console/Command/RabbitMqWorkersUpCommand.php
@@ -2,9 +2,7 @@
 
 namespace Console\Command;
 
-use ApiConsumer\Fetcher\FetcherService;
-use PhpAmqpLib\Connection\AMQPConnection;
-use PhpAmqpLib\Message\AMQPMessage;
+use Console\Worker\FetchLinksWorker;
 use Silex\Application;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -25,49 +23,15 @@ class RabbitMqWorkersUpCommand extends ApplicationAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
-        $queueName = 'fetch';
-        $exchange = 'social';
-
-        /** @var AMQPConnection $amqp */
         $amqp = $this->app['amqp'];
-        $channel = $amqp->channel();
+        $fetcher = $this->app['api_consumer.fetcher'];
+        $userProvider = $this->app['api_consumer.user_provider'];
 
-        $channel->queue_declare($queueName, false, true, false, false);
+        $fetchLinksWorker = new FetchLinksWorker($amqp, $fetcher, $userProvider);
+        $fetchLinksWorker->process();
 
-        $channel->queue_bind($queueName, $exchange);
-
-        $channel->basic_qos(null, 1, null);
-
-        $channel->basic_consume($queueName, '', false, false, false, false, array($this, 'processMessage'));
-
-        $output->writeln('[*] Waiting for messages. To exit press CTRL+C');
-
-        while (count($channel->callbacks)) {
-            $channel->wait();
-        }
-
-        $channel->close();
         $amqp->close();
 
     }
 
-    public function processMessage(AMQPMessage $message)
-    {
-        $messageBody = unserialize($message->body);
-        $resourceOwner = $messageBody['resourceOwner'];
-        $userId = $messageBody['userId'];
-
-        $userProvider = $this->app['api_consumer.user_provider'];
-        $user = $userProvider->getUsersByResource(
-            $resourceOwner,
-            $userId
-        );
-
-        /** @var FetcherService $fetcher */
-        $fetcher = $this->app['api_consumer.fetcher'];
-
-        $fetcher->fetch($user['id'], $resourceOwner);
-
-        $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
-    }
 }
