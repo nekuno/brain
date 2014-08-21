61792eb993189e36c0ebbe777553f6e03fec96b9
Juan Luis Martínez <juanlu@comakai.com>
2014-08-21 17:49:51 +0200

    Fix AuxMetadataParser

--- src/Tests/ApiConsumer/LinkProcessor/MetadataParser/AuxMetadataParser.php
+++ src/Tests/ApiConsumer/LinkProcessor/MetadataParser/AuxMetadataParser.php
@@ -6,42 +6,52 @@ namespace Tests\ApiConsumer\LinkProcessor\MetadataParser;
 
 use ApiConsumer\LinkProcessor\MetadataParser\MetadataParser;
 
-class AuxMetadataParser extends MetadataParser {
+class AuxMetadataParser extends MetadataParser
+{
 
-    public function setValidNameAttributeValues(array $names){
+    public function setValidNameAttributeValues(array $names)
+    {
         $this->validNameAttributeValues = $names;
     }
 
-    public function setValidRelAttributeValues(array $names){
+    public function setValidRelAttributeValues(array $names)
+    {
         $this->validRelAttributeValues = $names;
     }
 
-    public function keysAndValuesNotContentToLowercase(array $data){
+    public function keysAndValuesNotContentToLowercase($data)
+    {
         return parent::keysAndValuesNotContentToLowercase($data);
     }
 
-    public function isValidNameAttribute(array $data){
+    public function isValidNameAttribute($data)
+    {
         return parent::isValidNameAttribute($data);
     }
 
-    public function isValidRelAttribute(array $data){
+    public function isValidRelAttribute($data)
+    {
         return parent::isValidRelAttribute($data);
     }
 
-    public function removeUselessTags(array $tags)
+    public function removeUselessTags($tags)
     {
         return parent::removeUseLessTags($tags);
     }
 
-    public function hasOneUsefulAttributeAtLeast(array $tag){
-        return parent::hasOneUsefulAttributeAtLeast($tag);
+    public function hasOneUsefulAttributeAtLeast(array $data)
+    {
+        return parent::hasOneUsefulAttributeAtLeast($data
+        );
     }
 
-    public function isValidContentAttribute(array $tag){
+    public function isValidContentAttribute($tag)
+    {
         return parent::isValidContentAttribute($tag);
     }
 
-    public function removeTagsSorterThanNWords(array $tags, $numWords){
-        return parent::removeTagsSorterThanNWords($tags, $numWords);
+    public function removeTagsSorterThanNWords($tags, $wordLimit = 2)
+    {
+        return parent::removeTagsSorterThanNWords($tags, $wordLimit = 2);
     }
 }
