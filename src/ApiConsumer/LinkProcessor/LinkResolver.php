d8bd95ce8d8552d8f4d003ae2aa296b969b556e9
Juan Luis Martínez <juanlu@comakai.com>
2014-08-28 09:22:38 +0200

    QS-180: Always return an url

--- src/ApiConsumer/LinkProcessor/LinkResolver.php
+++ src/ApiConsumer/LinkProcessor/LinkResolver.php
@@ -32,7 +32,7 @@ class LinkResolver
             $crawler = $this->client->request('GET', $url);
             $statusCode = $this->client->getResponse()->getStatus();
         } catch (RequestException $e) {
-            return null;
+            return $url;
         }
 
         if ($statusCode == 200) {
@@ -52,7 +52,7 @@ class LinkResolver
             return $uri;
         }
 
-        return null;
+        return $url;
 
     }
 } 
