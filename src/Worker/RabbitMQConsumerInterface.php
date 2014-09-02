c3a45ec63787b169b58fc7a53c600e5bb1c4f485
Adrián Ríos Francia <adrian.web.dev@gmail.com>
2014-09-02 14:35:26 +0200

    QS-222 Added "callback" method to interface

--- src/Worker/RabbitMQConsumerInterface.php
+++ src/Worker/RabbitMQConsumerInterface.php
@@ -6,11 +6,24 @@
 namespace Worker;
 
 
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * Interface RabbitMQConsumerInterface
+ * @package Worker
+ */
 interface RabbitMQConsumerInterface {
 
     /**
-     * Starts to process queued tasks
+     * Starts to process queued messages
      */
     public function consume();
 
+    /**
+     * Process message
+     *
+     * @param AMQPMessage $message
+     * @return mixed
+     */
+    public function callback(AMQPMessage $message);
 }
