caeaeac5e9a8a77b039b4f8a4d11d90be5fc2ce3
Zaski <zaski123@gmail.com>
2014-09-25 05:22:06 +0200

    created function to set/update the parameters for the normal distribution for the matching by questions

--- src/Model/User/Matching/MatchingModel.php
+++ src/Model/User/Matching/MatchingModel.php
@@ -125,10 +125,42 @@ class MatchingModel
 
     public function updateQuestionsNormalDistributionVariables()
     {
+        //Construct query string
+        $queryString = "
+        MATCH
+            (u1:User),
+            (u2:User)
+        OPTIONAL MATCH
+            (u1)-[:ACCEPTS]->(commonanswer:Answer)<-[:ANSWERS]-(u2)
+        WITH
+            u1, u2, count(commonanswer) AS numOfCommonAnswers
+        RETURN
+            avg(numOfCommonAnswers) AS ave_questions,
+            stdevp(numOfCommonAnswers) AS stdev_questions
+        ";
+
+        //Create the Neo4j query object
+        $query = new Query(
+            $this->client,
+            $queryString
+        );
+
+        //Execute Query
+        try {
+            $result = $query->getResultSet();
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        //Get the wanted results
+        foreach ($result as $row) {
+            $average = $row['ave_questions'];
+            $stdev = $row['stdev_questions'];
+        }
 
         //Set the average and standard deviation for the content Normal Distribution
-        //$this->ave_questions = $average;
-        //$this->stdev_questions = $stdev;
+        $this->ave_questions = $average;
+        $this->stdev_questions = $stdev;
     }
 
     /**********************************************************************************************************************
