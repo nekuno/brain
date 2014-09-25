5f13a267050ee39306bbed2f2fd34c81f831d29c
Zaski <zaski123@gmail.com>
2014-09-25 06:56:50 +0200

    Added function to calculate and set the matching by questions between two users.

--- src/Model/User/Matching/MatchingModel.php
+++ src/Model/User/Matching/MatchingModel.php
@@ -166,5 +166,119 @@ class MatchingModel
     /**********************************************************************************************************************
      *
      */
+    public function getMatchingBetweenTwoUsersBasedOnAnswers($id1, $id2)
+    {
+        //Construct query String
+        $queryString = "
+        MATCH
+            (u1:User),
+            (u2:User)
+        WHERE
+            u1.qnoow_id = {id1} AND
+            u2.qnoow_id = {id2}
+        OPTIONAL MATCH
+            (u1)-[:ACCEPTS]->(commonanswer1:Answer)<-[:ANSWERS]-(u2),
+            (commonanswer1)-[:IS_ANSWER_OF]->(commonquestion1)<-[r1:RATES]-(u1)
+        OPTIONAL MATCH
+            (u2)-[:ACCEPTS]->(commonanswer2:Answer)<-[:ANSWERS]-(u1),
+            (commonanswer2)-[:IS_ANSWER_OF]->(commonquestion2)<-[r2:RATES]-(u2)
+        OPTIONAL MATCH
+            (u1)-[r3:RATES]->(:Question)<-[r4:RATES]-(u2)
+        WITH
+            u1, u2, (count(commonanswer1)+count(commonanswer2))/2 AS numOfCommonAnswers,
+            [n1 IN collect(distinct r1) |n1.rating] AS little1_elems,
+            [n2 IN collect(distinct r2) |n2.rating] AS little2_elems,
+            [n3 IN collect(distinct r3) |n3.rating] AS CIT1_elems,
+            [n4 IN collect(distinct r4) |n4.rating] AS CIT2_elems
+        WITH
+            u1, u2, numOfCommonAnswers,
+            tofloat( reduce(little1 = 0, n1 IN little1_elems | little1 + n1) ) AS little1,
+            tofloat( reduce(little2 = 0, n2 IN little2_elems | little2 + n2) ) AS little2,
+            tofloat( reduce(CIT1 = 0, n3 IN CIT1_elems | CIT1 + n3) ) AS CIT1,
+            tofloat( reduce(CIT1 = 0, n4 IN CIT2_elems | CIT1 + n4) ) AS CIT2
+        WITH
+            u1, u2, numOfCommonAnswers,
+            CASE
+                WHEN
+                    CIT1 > 0 AND CIT2 > 0
+                THEN
+                    sqrt( tofloat( little1/CIT1 ) * tofloat( little2/CIT2 ) )
+                 ELSE
+                    0
+            END
+            AS match_user1_user2
+        RETURN
+            match_user1_user2,
+            numOfCommonAnswers
+        ";
+
+        //State the value of the variables in the query string
+        $queryDataArray = array(
+            'id1' => $id1,
+            'id2' => $id2
+        );
+
+        //Construct query
+        $query = new Query(
+            $this->client,
+            $queryString,
+            $queryDataArray
+        );
+
+        //Execute query
+        try {
+            $result = $query->getResultSet();
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        //Get the wanted results
+        foreach($result as $row){
+            $ratingForMatching = $row['match_user1_user2'];
+            $normal_x = $row['numOfCommonAnswers'];
+        }
+
+        //Calculate the matching
+        $matching =
+            (
+                $ratingForMatching +
+                stats_dens_normal($normal_x, $this->ave_questions, $this->stdev_questions) //function from stats PHP extension
+            ) / 2;
+
+        //Query to create the matching relationship with the appropriate value
+
+        //Construct query String
+        $queryString = "
+        MATCH
+            (u1:User),
+            (u2:User)
+        WHERE
+            u1.qnoow_id = {id1} AND
+            u2.qnoow_id = {id2}
+        CREATE UNIQUE
+            (u1)-[m:MATCHES]-(u2)
+        SET
+            m.matching_questions = match_user1_user2,
+            m.timestamp_questions = timestamp()
+        RETURN
+            m
+        ";
+
+        //Construct query (queryDataArray is the same as before, so no need to re-set it)
+        $query = new Query(
+            $this->client,
+            $queryString,
+            $queryDataArray
+        );
+
+        //Execute query
+        try {
+            $result = $query->getResultSet();
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        return $matching;
+    }
 
 } 
