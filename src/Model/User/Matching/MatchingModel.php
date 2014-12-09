1744452459db52b242247f992e05dd0a6466d50d
Sergio Fernández <sergio@yoprogramador.com>
2014-12-09 19:26:49 +0100

    QS-411 Fix calculate matching based on questions

--- src/Model/User/Matching/MatchingModel.php
+++ src/Model/User/Matching/MatchingModel.php
@@ -355,11 +355,6 @@ class MatchingModel
     public function calculateMatchingBetweenTwoUsersBasedOnAnswers($id1, $id2)
     {
 
-        $data = $this->normalDistributionModel->getQuestionsNormalDistributionVariables();
-
-        $questionsAverage = $data->average;
-        $questionsStdev   = $data->stdev;
-
         //Construct query String
         $queryString = "
         MATCH
@@ -369,40 +364,100 @@ class MatchingModel
             u1.qnoow_id = {id1} AND
             u2.qnoow_id = {id2}
         OPTIONAL MATCH
-            (u1)-[:ACCEPTS]->(commonanswer1:Answer)<-[:ANSWERS]-(u2),
-            (commonanswer1)-[:IS_ANSWER_OF]->(commonquestion1)<-[r1:RATES]-(u1)
+            (u1)-[:ACCEPTS]->(acceptedAnswerU1:Answer)<-[:ANSWERS]-(u2),
+            (acceptedAnswerU1)-[:IS_ANSWER_OF]->(:Question)<-[rateAcceptedAnswerU1:RATES]-(u1)
         OPTIONAL MATCH
-            (u2)-[:ACCEPTS]->(commonanswer2:Answer)<-[:ANSWERS]-(u1),
-            (commonanswer2)-[:IS_ANSWER_OF]->(commonquestion2)<-[r2:RATES]-(u2)
+            (u2)-[:ACCEPTS]->(acceptedAnswerU2:Answer)<-[:ANSWERS]-(u1),
+            (acceptedAnswerU2)-[:IS_ANSWER_OF]->(:Question)<-[rateAcceptedAnswerU2:RATES]-(u2)
         OPTIONAL MATCH
-            (u1)-[r3:RATES]->(:Question)<-[r4:RATES]-(u2)
+            (u1)-[rateCommonAnswerU1:RATES]->(commonQuestions:Question)<-[rateCommonAnswerU2:RATES]-(u2)
         WITH
-            u1, u2,
-            (count(commonanswer1)+count(commonanswer2))/2 AS numOfCommonAnswers,
-            [n1 IN collect(distinct r1) |n1.rating] AS little1_elems,
-            [n2 IN collect(distinct r2) |n2.rating] AS little2_elems,
-            [n3 IN collect(distinct r3) |n3.rating] AS CIT1_elems,
-            [n4 IN collect(distinct r4) |n4.rating] AS CIT2_elems
+            count(DISTINCT commonQuestions) AS numOfCommonQuestions,
+            [n1 IN collect(distinct rateAcceptedAnswerU1) |
+                CASE toint(n1.rating)
+			        WHEN 0 THEN 0
+			        WHEN 1 THEN 1
+			        WHEN 2 THEN 10
+			        WHEN 3 THEN 50
+			        ELSE 0
+		        END
+            ] AS arrayRatingAcceptedAnswerU1,
+            [n2 IN collect(distinct rateAcceptedAnswerU2) |
+                CASE toint(n2.rating)
+			        WHEN 0 THEN 0
+			        WHEN 1 THEN 1
+			        WHEN 2 THEN 10
+			        WHEN 3 THEN 50
+			        ELSE 0
+                END
+            ] AS arrayRatingAcceptedAnswerU2,
+            [n3 IN collect(distinct rateCommonAnswerU1) |
+                CASE toint(n3.rating)
+			        WHEN 0 THEN 0
+			        WHEN 1 THEN 1
+			        WHEN 2 THEN 10
+			        WHEN 3 THEN 50
+			        ELSE 0
+                END
+            ] AS arrayRatingCommonAnswerU1,
+            [n4 IN collect(distinct rateCommonAnswerU2) |
+                CASE toint(n4.rating)
+			        WHEN 0 THEN 0
+			        WHEN 1 THEN 1
+			        WHEN 2 THEN 10
+			        WHEN 3 THEN 50
+			        ELSE 0
+                END
+            ] AS arrayRatingCommonAnswerU2
         WITH
-            u1, u2, numOfCommonAnswers,
-            tofloat( reduce(little1 = 0, n1 IN little1_elems | little1 + n1) ) AS little1,
-            tofloat( reduce(little2 = 0, n2 IN little2_elems | little2 + n2) ) AS little2,
-            tofloat( reduce(CIT1 = 0, n3 IN CIT1_elems | CIT1 + n3) ) AS CIT1,
-            tofloat( reduce(CIT1 = 0, n4 IN CIT2_elems | CIT1 + n4) ) AS CIT2
+            tofloat(numOfCommonQuestions) as numOfCommonQuestions,
+            tofloat(
+                reduce(
+                    totalRatingAcceptedAnswersU1 = 0,
+                    n1 IN arrayRatingAcceptedAnswerU1 | totalRatingAcceptedAnswersU1 + n1
+                )
+            ) AS totalRatingAcceptedAnswersU1,
+            tofloat(
+                reduce(
+                    totalRatingAcceptedAnswersU2 = 0,
+                    n2 IN arrayRatingAcceptedAnswerU2 | totalRatingAcceptedAnswersU2 + n2
+                )
+            ) AS totalRatingAcceptedAnswersU2,
+            tofloat(
+                reduce(
+                    totalRatingCommonAnswersU1 = 0,
+                    n3 IN arrayRatingCommonAnswerU1 | totalRatingCommonAnswersU1 + n3
+                )
+            ) AS totalRatingCommonAnswersU1,
+            tofloat(
+                reduce(
+                    totalRatingCommonAnswersU2 = 0,
+                    n4 IN arrayRatingCommonAnswerU2 | totalRatingCommonAnswersU2 + n4
+                )
+            ) AS totalRatingCommonAnswersU2
         WITH
-            u1, u2, numOfCommonAnswers,
+	        CASE
+                WHEN numOfCommonQuestions > 0 THEN
+	                1 - (1 / numOfCommonQuestions)
+		        ELSE tofloat(0)
+	        END AS error,
             CASE
-                WHEN
-                    CIT1 > 0 AND CIT2 > 0
-                THEN
-                    sqrt( tofloat( little1/CIT1 ) * tofloat( little2/CIT2 ) )
-                ELSE
-                    0
-            END
-            AS match_user1_user2
+                WHEN totalRatingCommonAnswersU1 > 0 AND totalRatingCommonAnswersU2 > 0 THEN
+		            tofloat(
+		                sqrt(
+		                    (totalRatingAcceptedAnswersU1/totalRatingCommonAnswersU1) *
+		                    (totalRatingAcceptedAnswersU2/totalRatingCommonAnswersU2)
+                        )
+                    )
+                ELSE tofloat(0)
+            END AS rawMatching
+        WITH
+	        CASE
+		        WHEN error < rawMatching THEN error
+		        ELSE rawMatching
+            END AS matching
         RETURN
-            match_user1_user2,
-            numOfCommonAnswers
+            matching
         ";
 
         //State the value of the variables in the query string
@@ -425,28 +480,13 @@ class MatchingModel
             throw $e;
         }
 
-        //Get the wanted results
-        $ratingForMatching = 0;
-        $normalX           = 0;
+        $matching = 0;
         foreach ($result as $row) {
-            $ratingForMatching = $row['match_user1_user2'];
-            $normalX           = $row['numOfCommonAnswers'];
+            $matching = $row['matching'];
         }
 
-        //Calculate the matching
-        $matching =
-            (
-                $ratingForMatching +
-                stats_dens_normal($normalX, $questionsAverage, $questionsStdev) //function from stats PHP extension
-            ) / 2;
+        //Create the matching relationship with the appropriate value
 
-        if ($matching == false) {
-            $matching = 0;
-        }
-
-        //Query to create the matching relationship with the appropriate value
-
-        //Construct query String
         $queryString = "
         MATCH
             (u1:User),
