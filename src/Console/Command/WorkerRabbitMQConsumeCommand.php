582181b50d3f9b5d5d86361d88479d08ad3c2dd0
Juan Luis Martínez <juanlu@comakai.com>
2015-02-13 12:23:21 +0100

    QS-506: Debug workers

--- src/Console/Command/WorkerRabbitMQConsumeCommand.php
+++ src/Console/Command/WorkerRabbitMQConsumeCommand.php
@@ -3,14 +3,19 @@
 namespace Console\Command;
 
 use ApiConsumer\Auth\UserProviderInterface;
+use ApiConsumer\EventListener\FetchLinksSubscriber;
 use ApiConsumer\Fetcher\FetcherService;
 use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use Silex\Application;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Worker\LinkProcessorWorker;
 use Worker\MatchingCalculatorWorker;
 
@@ -31,12 +36,13 @@ class WorkerRabbitMQConsumeCommand extends ApplicationAwareCommand
     {
 
         $this->setName('worker:rabbitmq:consume')
-             ->setDescription("Start RabbitMQ consumer by name")
-             ->setDefinition(
-                 array(
-                     new InputArgument('consumer', InputArgument::OPTIONAL, 'Consumer to start up', 'fetching')
-                 )
-             );
+            ->setDescription("Start RabbitMQ consumer by name")
+            ->setDefinition(
+                array(
+                    new InputArgument('consumer', InputArgument::OPTIONAL, 'Consumer to start up', 'fetching'),
+                    new InputOption('debug', null, InputOption::VALUE_NONE, 'Debug the process to the console'),
+                )
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -48,32 +54,47 @@ class WorkerRabbitMQConsumeCommand extends ApplicationAwareCommand
             throw new \Exception('Invalid consumer name');
         }
 
-        /** @var LoggerInterface $logger */
+        /* @var $logger LoggerInterface */
         $logger = $this->app['monolog'];
-
-        /** @var UserProviderInterface $userProvider */
+        /* @var $userProvider UserProviderInterface */
         $userProvider = $this->app['api_consumer.user_provider'];
-        /** @var FetcherService $fetcher */
+        /* @var $fetcher FetcherService */
         $fetcher = $this->app['api_consumer.fetcher'];
 
-        /** @var AMQPStreamConnection $connection */
+        if ($input->getOption('debug')) {
+
+            $verbosityLevelMap = array(
+                LogLevel::NOTICE => OutputInterface::VERBOSITY_NORMAL,
+                LogLevel::INFO => OutputInterface::VERBOSITY_NORMAL,
+            );
+            $logger = new ConsoleLogger($output, $verbosityLevelMap);
+
+            $fetchLinksSubscriber = new FetchLinksSubscriber($output);
+            $dispatcher = $this->app['dispatcher'];
+            /* @var $dispatcher EventDispatcher */
+            $dispatcher->addSubscriber($fetchLinksSubscriber);
+        }
+
+        $fetcher->setLogger($logger);
+
+        /* @var  $connection AMQPStreamConnection */
         $connection = $this->app['amqp'];
 
         $output->writeln(sprintf('Starting %s consumer', $consumer));
         switch ($consumer) {
             case 'fetching':
-                /** @var AMQPChannel $channel */
+                /* @var $channel AMQPChannel */
                 $channel = $connection->channel();
-                $worker  = new LinkProcessorWorker($channel, $fetcher, $userProvider);
+                $worker = new LinkProcessorWorker($channel, $fetcher, $userProvider);
                 $worker->setLogger($logger);
                 $logger->info('Processing fetching queue');
                 $worker->consume();
                 $channel->close();
                 break;
             case 'matching':
-                /** @var AMQPChannel $channel */
+                /* @var $channel AMQPChannel */
                 $channel = $connection->channel();
-                $worker  = new MatchingCalculatorWorker($channel, $this->app['users.model'], $this->app['users.matching.model'], $this->app['users.similarity.model']);
+                $worker = new MatchingCalculatorWorker($channel, $this->app['users.model'], $this->app['users.matching.model'], $this->app['users.similarity.model']);
                 $worker->setLogger($logger);
                 $logger->info('Processing matching queue');
                 $worker->consume();
