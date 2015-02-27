b11c6aba5a847c59425b4926c892a0008157273b
Juan Luis Martínez <juanlu@comakai.com>
2015-02-27 17:43:40 +0100

    QS-506: Fix relative canonicals and improve LinkResolver

--- src/Tests/ApiConsumer/LinkProcessor/LinkResolverTest.php
+++ src/Tests/ApiConsumer/LinkProcessor/LinkResolverTest.php
@@ -21,49 +21,61 @@ class LinkResolverTest extends \PHPUnit_Framework_TestCase
         $client
             ->expects($this->once())
             ->method('request')
-            ->will($this->returnCallback(function () {
+            ->will(
+                $this->returnCallback(
+                    function () {
 
-                $crawler = $this->getMockBuilder('Symfony\Component\DomCrawler\Crawler')->getMock();
+                        $crawler = $this->getMockBuilder('Symfony\Component\DomCrawler\Crawler')->getMock();
 
-                $crawler->expects($this->any())
-                    ->method('filterXPath')
-                    ->will($this->returnSelf());
-                $crawler->expects($this->any())
-                    ->method('attr')
-                    ->will($this->returnValue(null));
+                        $crawler->expects($this->any())
+                            ->method('filterXPath')
+                            ->will($this->returnSelf());
+                        $crawler->expects($this->any())
+                            ->method('attr')
+                            ->will($this->returnValue(null));
 
-                return $crawler;
-            }));
+                        return $crawler;
+                    }
+                )
+            );
 
         $client
             ->expects($this->once())
             ->method('getResponse')
-            ->will($this->returnCallback(function () {
+            ->will(
+                $this->returnCallback(
+                    function () {
 
-                $response = $this->getMockBuilder('Symfony\Component\BrowserKit\Response')->getMock();
+                        $response = $this->getMockBuilder('Symfony\Component\BrowserKit\Response')->getMock();
 
-                $response->expects($this->once())
-                    ->method('getStatus')
-                    ->will($this->returnValue(200));
+                        $response->expects($this->once())
+                            ->method('getStatus')
+                            ->will($this->returnValue(200));
 
-                return $response;
-            }));
+                        return $response;
+                    }
+                )
+            );
 
         $client
             ->expects($this->once())
             ->method('getRequest')
-            ->will($this->returnCallback(function () use ($resolved) {
+            ->will(
+                $this->returnCallback(
+                    function () use ($resolved) {
 
-                $request = $this->getMockBuilder('Symfony\Component\BrowserKit\Request')
-                    ->disableOriginalConstructor()
-                    ->getMock();
+                        $request = $this->getMockBuilder('Symfony\Component\BrowserKit\Request')
+                            ->disableOriginalConstructor()
+                            ->getMock();
 
-                $request->expects($this->once())
-                    ->method('getUri')
-                    ->will($this->returnValue($resolved));
+                        $request->expects($this->once())
+                            ->method('getUri')
+                            ->will($this->returnValue($resolved));
 
-                return $request;
-            }));
+                        return $request;
+                    }
+                )
+            );
 
         $linkResolver = new LinkResolver($client);
 
@@ -80,16 +92,16 @@ class LinkResolverTest extends \PHPUnit_Framework_TestCase
 
         $crawler = $this->getMockBuilder('Symfony\Component\DomCrawler\Crawler')->getMock();
 
-        $crawler->expects($this->exactly(2))
+        $crawler->expects($this->any())
             ->method('filterXPath')
             ->will($this->returnSelf());
-        $crawler->expects($this->exactly(2))
+        $crawler->expects($this->any())
             ->method('attr')
-            ->will($this->onConsecutiveCalls($canonical, null));
+            ->will($this->returnValue($canonical));
 
         $response = $this->getMockBuilder('Symfony\Component\BrowserKit\Response')->getMock();
 
-        $response->expects($this->exactly(2))
+        $response->expects($this->any())
             ->method('getStatus')
             ->will($this->returnValue(200));
 
@@ -97,24 +109,77 @@ class LinkResolverTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $request->expects($this->exactly(2))
+        $request->expects($this->any())
             ->method('getUri')
-            ->will($this->onConsecutiveCalls($resolved, $canonical));
+            ->will($this->returnValue($resolved));
 
         $client = $this->getMockBuilder('Goutte\Client')->getMock();
 
         $client
-            ->expects($this->exactly(2))
+            ->expects($this->any())
             ->method('request')
             ->will($this->returnValue($crawler));
 
         $client
-            ->expects($this->exactly(2))
+            ->expects($this->any())
             ->method('getResponse')
             ->will($this->returnValue($response));
 
         $client
-            ->expects($this->exactly(2))
+            ->expects($this->any())
+            ->method('getRequest')
+            ->will($this->returnValue($request));
+
+        $linkResolver = new LinkResolver($client);
+
+        $this->assertEquals($canonical, $linkResolver->resolve($target));
+    }
+
+    public function testResolveValidUrlWithRelativeCanonical()
+    {
+
+        $target = 'http://bit.ly/1LPQl45';
+        $resolved = 'https://vimeo.com/channels/staffpicks/120559169';
+        $relativeCanonical = '/120559169';
+        $canonical = 'https://vimeo.com/120559169';
+
+        $crawler = $this->getMockBuilder('Symfony\Component\DomCrawler\Crawler')->getMock();
+
+        $crawler->expects($this->any())
+            ->method('filterXPath')
+            ->will($this->returnSelf());
+        $crawler->expects($this->any())
+            ->method('attr')
+            ->will($this->returnValue($relativeCanonical));
+
+        $response = $this->getMockBuilder('Symfony\Component\BrowserKit\Response')->getMock();
+
+        $response->expects($this->any())
+            ->method('getStatus')
+            ->will($this->returnValue(200));
+
+        $request = $this->getMockBuilder('Symfony\Component\BrowserKit\Request')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $request->expects($this->any())
+            ->method('getUri')
+            ->will($this->returnValue($resolved));
+
+        $client = $this->getMockBuilder('Goutte\Client')->getMock();
+
+        $client
+            ->expects($this->any())
+            ->method('request')
+            ->will($this->returnValue($crawler));
+
+        $client
+            ->expects($this->any())
+            ->method('getResponse')
+            ->will($this->returnValue($response));
+
+        $client
+            ->expects($this->any())
             ->method('getRequest')
             ->will($this->returnValue($request));
 
@@ -133,33 +198,41 @@ class LinkResolverTest extends \PHPUnit_Framework_TestCase
         $client
             ->expects($this->once())
             ->method('getResponse')
-            ->will($this->returnCallback(function () {
+            ->will(
+                $this->returnCallback(
+                    function () {
 
-                $response = $this->getMockBuilder('Symfony\Component\BrowserKit\Response')
-                    ->getMock();
+                        $response = $this->getMockBuilder('Symfony\Component\BrowserKit\Response')
+                            ->getMock();
 
-                $response->expects($this->once())
-                    ->method('getStatus')
-                    ->will($this->returnValue(404));
+                        $response->expects($this->once())
+                            ->method('getStatus')
+                            ->will($this->returnValue(404));
 
-                return $response;
-            }));
+                        return $response;
+                    }
+                )
+            );
 
         $client
             ->expects($this->once())
             ->method('getRequest')
-            ->will($this->returnCallback(function () use ($target) {
+            ->will(
+                $this->returnCallback(
+                    function () use ($target) {
 
-                $request = $this->getMockBuilder('Symfony\Component\BrowserKit\Request')
-                    ->disableOriginalConstructor()
-                    ->getMock();
+                        $request = $this->getMockBuilder('Symfony\Component\BrowserKit\Request')
+                            ->disableOriginalConstructor()
+                            ->getMock();
 
-                $request->expects($this->once())
-                    ->method('getUri')
-                    ->will($this->returnValue($target));
+                        $request->expects($this->once())
+                            ->method('getUri')
+                            ->will($this->returnValue($target));
 
-                return $request;
-            }));
+                        return $request;
+                    }
+                )
+            );
 
         $linkResolver = new LinkResolver($client);
 
@@ -176,28 +249,36 @@ class LinkResolverTest extends \PHPUnit_Framework_TestCase
         $client
             ->expects($this->once())
             ->method('request')
-            ->will($this->returnCallback(function () {
-                $exception = $this->getMockBuilder('GuzzleHttp\Exception\RequestException')
-                    ->disableOriginalConstructor()
-                    ->getMock();
-                throw $exception;
-            }));
+            ->will(
+                $this->returnCallback(
+                    function () {
+                        $exception = $this->getMockBuilder('GuzzleHttp\Exception\RequestException')
+                            ->disableOriginalConstructor()
+                            ->getMock();
+                        throw $exception;
+                    }
+                )
+            );
 
         $client
             ->expects($this->once())
             ->method('getRequest')
-            ->will($this->returnCallback(function () use ($target) {
+            ->will(
+                $this->returnCallback(
+                    function () use ($target) {
 
-                $request = $this->getMockBuilder('Symfony\Component\BrowserKit\Request')
-                    ->disableOriginalConstructor()
-                    ->getMock();
+                        $request = $this->getMockBuilder('Symfony\Component\BrowserKit\Request')
+                            ->disableOriginalConstructor()
+                            ->getMock();
 
-                $request->expects($this->once())
-                    ->method('getUri')
-                    ->will($this->returnValue($target));
+                        $request->expects($this->once())
+                            ->method('getUri')
+                            ->will($this->returnValue($target));
 
-                return $request;
-            }));
+                        return $request;
+                    }
+                )
+            );
 
         $linkResolver = new LinkResolver($client);
 
