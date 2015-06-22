fa81675aba2c67f8cd6df07f2467f9a353b923c3
Roberto Martinez <yawmoght@gmail.com>
2015-06-22 11:42:32 +0200

    Modify command to analyze link by link

--- src/Console/Command/LinksFindDuplicatesCommand.php
+++ src/Console/Command/LinksFindDuplicatesCommand.php
@@ -18,14 +18,38 @@ class LinksFindDuplicatesCommand extends ApplicationAwareCommand
         $this->setName('links:find-duplicates')
             ->setDescription("Return links with identical URLs")
             ->addOption('fuse', null, InputOption::VALUE_NONE, 'Automatically fuse found duplicates')
+            ->addOption('step', null, InputOption::VALUE_NONE, 'Obtain, output and/or fuse duplicate by duplicate')
+            ->addOption('offset', null, InputOption::VALUE_OPTIONAL, 'Links to skip if using step option', 0)
             ->addOption('pseudoduplicates', null, InputOption::VALUE_NONE, 'Find nearly identical URLs instead of totally identical URLs');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
+        $output->writeln('Starting database search.');
+
         /* @var $linkModel LinkModel */
         $linkModel = $this->app['links.model'];
+
+        if ($input->getOption('pseudoduplicates') && $input->getOption('step')) {
+            $moreLinksExist = true;
+            $offset = $input->getOption('offset');
+            while ($moreLinksExist) {
+                $nextDuplicates = $linkModel->findPseudoduplicatesFromOffset((integer)$offset);
+                if ($nextDuplicates === null) {
+                    $output->writeln('No more links to analyze.');
+                    return;
+                }
+                if (empty($nextDuplicates)) {
+                    $output->writeln(sprintf('No pseudoduplicates found for link with offset %d', $offset));
+                } else {
+                    $output->writeln(sprintf('%d duplicated links found for links with id offset.', count($nextDuplicates), $offset));
+                    $this->analyzeDuplicates($nextDuplicates, $input, $output);
+                }
+                $offset++;
+            }
+            return;
+        }
 
         if ($input->getOption('pseudoduplicates')) {
             $duplicates = $linkModel->findPseudoduplicates();
@@ -38,42 +62,54 @@ class LinksFindDuplicatesCommand extends ApplicationAwareCommand
         if ($numDuplicates > 0) {
             $output->writeln(sprintf('%d duplicated links found.', $numDuplicates));
 
-            /* @var $gm GraphManager */
-            $gm = $this->app['neo4j.graph_manager'];
-
-            foreach ($duplicates as $duplicate) {
-                $mainurl = $duplicate['main']['url'];
-                $mainid = (integer)$duplicate['main']['id'];
-                $duplicateurl = $duplicate['duplicate']['url'];
-                $duplicateid = (integer)$duplicate['duplicate']['id'];
-
-                $output->writeln('Link with id ' . $duplicateid . ' and url ' . $duplicateurl .
-                    ' is a duplicate of link with id ' . $mainid . ' and url ' . $mainurl);
-
-                if ($input->getOption('fuse')) {
-                    $output->writeln('Fusing duplicate into main node');
-
-                    $fusion = $gm->fuseNodes($duplicateid, $mainid);
-                    /** @var ResultSet $deletionrs */
-                    $deletionrs = $fusion['deleted'];
-                    if ($deletionrs->count() > 0) {
-                        $output->writeln('Duplicate and main node successfully fused');
-                    } else {
-                        $output->writeln('Nodes were not fused');
-                    }
-                    $output->writeln('Cleaning inconsistencies');
-                    $cleaned = $linkModel->cleanInconsistencies($mainid);
-                    if (OutputInterface::VERBOSITY_NORMAL < $output->getVerbosity()) {
-                        $output->writeln('Like-dislike conflicts solved: ' . $cleaned['dislikes']);
-                        $output->writeln('Like-affinity conflicts solved: ' . $cleaned['affinities']);
-                    }
-                }
-            }
+            $this->analyzeDuplicates($duplicates, $input, $output);
 
         } else {
             $output->writeln('No duplicated links found.');
         }
 
         $output->writeln('Done.');
+    }
+
+    /**
+     * @param array $duplicates
+     * @param $input InputInterface
+     * @param $output OutputInterface
+     */
+    private function analyzeDuplicates(array $duplicates, $input, $output)
+    {
+        /* @var $gm GraphManager */
+        $gm = $this->app['neo4j.graph_manager'];
+        /* @var $linkModel LinkModel */
+        $linkModel = $this->app['links.model'];
+
+        foreach ($duplicates as $duplicate) {
+            $mainURL = $duplicate['main']['url'];
+            $mainId = (integer)$duplicate['main']['id'];
+            $duplicateURL = $duplicate['duplicate']['url'];
+            $duplicateId = (integer)$duplicate['duplicate']['id'];
+
+            $output->writeln('Link with id ' . $duplicateId . ' and url ' . $duplicateURL .
+                ' is a duplicate of link with id ' . $mainId . ' and url ' . $mainURL);
+
+            if ($input->getOption('fuse')) {
+                $output->writeln('Fusing duplicate into main node');
+
+                $fusion = $gm->fuseNodes($duplicateId, $mainId);
+                /** @var ResultSet $deletionRS */
+                $deletionRS = $fusion['deleted'];
+                if ($deletionRS->count() > 0) {
+                    $output->writeln('Duplicate and main node successfully fused');
+                } else {
+                    $output->writeln('Nodes were not fused');
+                }
+                $output->writeln('Cleaning inconsistencies');
+                $cleaned = $linkModel->cleanInconsistencies($mainId);
+                if (OutputInterface::VERBOSITY_NORMAL < $output->getVerbosity()) {
+                    $output->writeln('Like-dislike conflicts solved: ' . $cleaned['dislikes']);
+                    $output->writeln('Like-affinity conflicts solved: ' . $cleaned['affinities']);
+                }
+            }
+        }
     }
 }
--- src/Model/LinkModel.php
+++ src/Model/LinkModel.php
@@ -661,6 +661,64 @@ class LinkModel
     }
 
     /**
+     * @param int $offset
+     * @return array with duplicates, or null if offset > links in database
+     * @throws \Exception
+     */
+    public function findPseudoduplicatesFromOffset($offset = 0)
+    {
+        $qb = $this->gm->createQueryBuilder();
+
+        $qb->setParameter('offset', (integer)$offset);
+        $qb->match('(l1:Link)')
+            ->with('l1')
+            ->orderBy('id(l1) ASC')//in case new links are added while iterating this function
+            ->skip('{offset}')
+            ->limit('1')
+            ->optionalMatch('(l2:Link)')
+            ->where('(id(l2)>id(l1))',
+                '(l2.url=l1.url+"/" OR l2.url=l1.url+"?" OR l2.url=l1.url+"&")')
+            ->optionalMatch('(l3:Link)')
+            ->where('(id(l3)>id(l1))',
+                '(l1.url=l3.url+"/" OR l1.url=l3.url+"?" OR l1.url=l3.url+"&")')
+            ->returns('id(l1) AS id1, l1.url AS url1,
+                        collect(id(l2)) AS id2, collect(l2.url) AS url2,
+                        collect(id(l3)) AS id3, collect(l3.url) AS url3');
+        $rs = $qb->getQuery()->getResultSet();
+
+        if ($rs->count() != 0) {
+            /** @var $row Row */
+            $row = $rs->current();
+            $result=array();
+            $pseudoduplicatesIds = $row->offsetGet('id2');
+            $pseudoduplicatesUrls = $row->offsetGet('url2');
+            $pseudoduplicatedIds = $row->offsetGet('url3');
+            $pseudoduplicatedUrls = $row->offsetGet('url3');
+            for ($i = 0; $i < count($pseudoduplicatesIds); $i++) {
+                $duplicate = array();
+                $duplicate['main'] = array('id' => $row->offsetGet('id1'),
+                    'url' => $row->offsetGet('url1'));
+                $duplicate['duplicate'] = array('id' => $pseudoduplicatesIds[$i],
+                    'url' => $pseudoduplicatesUrls[$i]);
+                $result[] = $duplicate;
+            }
+
+            for ($i = 0; $i < count($pseudoduplicatedIds); $i++) {
+                $duplicate = array();
+                $duplicate['main'] = array('id' => $pseudoduplicatedIds[$i],
+                    'url' => $pseudoduplicatedUrls[$i]);
+                $duplicate['duplicate'] = array('id' => $row->offsetGet('id1'),
+                    'url' => $row->offsetGet('url1'));
+                $result[] = $duplicate;
+            }
+        } else {
+            $result=null;
+        }
+
+        return $result;
+    }
+
+    /**
      * @param $url
      * @return boolean
      */
