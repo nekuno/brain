356ee9a623768e0355eab19c6325c2870ad3ff83
Manolo Salsas <manolez@gmail.com>
2015-08-28 11:20:39 +0200

    QS-755: Fix white spaces in social network name

--- src/Model/User/LookUpModel.php
+++ src/Model/User/LookUpModel.php
@@ -236,7 +236,8 @@ class LookUpModel
         $counter = 0;
         foreach($socialProfiles as $resource => $url) {
             $counter++;
-            $resourceNode = ucfirst(str_replace('.', '', $resource)) . 'SocialNetwork';
+            $label = ucfirst(str_replace('.', '', str_replace(' ', '', $resource)));
+            $resourceNode = $label . 'SocialNetwork';
             $qb->merge('(sn' . $counter . ':SocialNetwork:' . $resourceNode . ')')
                 ->merge('(u)-[hsn' . $counter . ':HAS_SOCIAL_NETWORK {url: { url' . $counter . ' }}]->(sn' . $counter . ')')
                 ->setParameter('url' . $counter, $url);
