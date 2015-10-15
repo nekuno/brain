005096bc949bb9a10b301ef6d9746d68225bfede
Manolo Salsas <manolez@gmail.com>
2015-10-15 18:06:31 +0200

    Try to start Neo4j if can't connect to vagrant machine

--- web/index_test.php
+++ web/index_test.php
@@ -7,13 +7,13 @@ putenv("APP_ENV=test");
 $neo4jVersion = '2.2.5';
 
 require_once __DIR__.'/../bootstrap-neo4j-testing.php';
-require_once __DIR__.'/../vendor/autoload.php';
+//require_once __DIR__.'/../vendor/autoload.php';
 
 Debug::enable();
 
 
-/* TODO: Uncomment to delete Neo4j after all tests
-register_shutdown_function('removeNeo4j', $neo4jVersion);
+// TODO: Uncomment to delete Neo4j after all tests
+/*register_shutdown_function('removeNeo4j', $neo4jVersion);
 
 function removeNeo4j($neo4jVersion)
 {
