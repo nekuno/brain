a3b7588f975ee94027bf48aa0cde86af8b9e15b8
Manolo Salsas <manolez@gmail.com>
2015-10-15 13:32:52 +0200

    QS-798: Create Neo4j instance from inside the project

--- web/index_test.php
+++ web/index_test.php
@@ -4,6 +4,19 @@ use Symfony\Component\Debug\Debug;
 
 putenv("APP_ENV=test");
 
+$neo4jVersion = '2.2.5';
+
+require_once __DIR__.'/../bootstrap-neo4j-testing.php';
 require_once __DIR__.'/../vendor/autoload.php';
 
 Debug::enable();
+
+
+/* TODO: Uncomment to delete Neo4j after all tests
+register_shutdown_function('removeNeo4j', $neo4jVersion);
+
+function removeNeo4j($neo4jVersion)
+{
+    exec("rm -rf neo4j-community-{$neo4jVersion}");
+}
+*/
