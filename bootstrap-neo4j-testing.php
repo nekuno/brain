6407d19075b49039f779225d89f3d6547e39ae88
Manolo Salsas <manolez@gmail.com>
2015-10-16 17:04:15 +0200

    QS-798: Disable security.auth

--- bootstrap-neo4j-testing.php
+++ bootstrap-neo4j-testing.php
@@ -9,6 +9,7 @@ if(! is_dir("vendor/neo4j-community-{$neo4jVersion}")) {
     exec("sed -i 's/#org.neo4j.server.webserver.address=0.0.0.0/org.neo4j.server.webserver.address=0.0.0.0/g' vendor/neo4j-community-{$neo4jVersion}/conf/neo4j-server.properties");
     exec("sed -i 's/org.neo4j.server.webserver.port=7474/org.neo4j.server.webserver.port=7475/g' vendor/neo4j-community-{$neo4jVersion}/conf/neo4j-server.properties");
     exec("sed -i 's/org.neo4j.server.webserver.https.port=7473/org.neo4j.server.webserver.https.port=7476/g' vendor/neo4j-community-{$neo4jVersion}/conf/neo4j-server.properties");
+    exec("sed -i 's/dbms.security.auth_enabled=true/dbms.security.auth_enabled=false/g' vendor/neo4j-community-{$neo4jVersion}/conf/neo4j-server.properties");
     exec("mkdir vendor/neo4j-community-".$neo4jVersion."/data/dbms");
     exec('echo "admin:SHA-256,5F5E0B0CEBC8A4FCEEAC6A1E3A778039C7D9912F6C0378CCCBC1F2B1479FCFEA,5DE083E710DD6E7341331DEB11D636D9:" > vendor/neo4j-community-'.$neo4jVersion.'/data/dbms/auth');
 }
