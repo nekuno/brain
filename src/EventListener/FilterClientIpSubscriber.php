6e09f231ad76bb7d232bda85b2610f2f06037374
Juan Luis Martínez <juanlu@comakai.com>
2015-12-22 11:15:44 +0100

    QS-894: Public actions

--- src/EventListener/FilterClientIpSubscriber.php
+++ src/EventListener/FilterClientIpSubscriber.php
@@ -4,7 +4,6 @@
  */
 namespace EventListener;
 
-use Controller\User\LookUpController;
 use Firebase\JWT\JWT;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
@@ -43,21 +42,22 @@ class FilterClientIpSubscriber implements EventSubscriberInterface
             return;
         }
 
-        if ($controller[0] instanceof LookUpController &&
-            $request->attributes->get('_controller') === 'lookUp.controller:setFromWebHookAction'
-        ) {
-            // TODO: Make this controller public
-        } elseif ($request->headers->has('authorization')) {
+        $publicActions = array(
+            'lookUp.controller:setFromWebHookAction',
+            'auth.controller:loginAction'
+        );
+
+        if ($request->headers->has('authorization')) {
             list($jwt) = sscanf($request->headers->get('authorization'), 'Bearer %s');
             try {
                 JWT::decode($jwt, $this->secret, array('HS256'));
             } catch (\Exception $e) {
                 throw new UnauthorizedHttpException('', 'JWT token not valid');
             }
-        } else {
-            if (!in_array($event->getRequest()->getClientIp(), $this->validIps)) {
-                throw new AccessDeniedHttpException('Access forbidden');
-            }
+        } elseif (in_array($request->attributes->get('_controller'), $publicActions)) {
+            return;
+        } elseif (!in_array($event->getRequest()->getClientIp(), $this->validIps)) {
+            throw new AccessDeniedHttpException('Access forbidden');
         }
     }
 }
