5ceda39bba540484322dc29ea989b12d93b143b9
Roberto Martinez <yawmoght@gmail.com>
2016-02-03 19:45:31 +0100

    QS-933: Filter node capabilities

---
+++ src/Model/User/Filters/FilterUsers.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @author yawmoght <yawmoght@gmail.com>
+ */
+
+namespace Model\User\Filters;
+
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class FilterUsers
+{
+
+    protected $metadata = array();
+
+    protected $profileFilters = array();
+    protected $usersFilters = array();
+    protected $id;
+    /**
+     * Filters constructor.
+     * @param $metadata
+     */
+    public function __construct($metadata)
+    {
+        $this->metadata = $metadata;
+    }
+
+
+
+    /**
+     * @param $name
+     * @return mixed|null
+     */
+    public function get($name)
+    {
+        if (!isset($metadata[$name])){
+            throw new Exception(sprintf('Trying to get metadata %s but it does not exist', $name));
+        }
+
+        if(isset($filters[$name])){
+            return $filters[$name];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function set($name, $value)
+    {
+        if (!isset($metadata[$name])){
+            throw new Exception(sprintf('Trying to set metadata %s but it does not exist', $name));
+        }
+
+        $this->metadata[$name] = $value;
+    }
+
+    public function getProfileFilters()
+    {
+        return $this->profileFilters;
+    }
+
+    public function getUserFilters()
+    {
+        return $this->usersFilters;
+    }
+
+    /**
+     * @param array $profileFilters
+     */
+    public function setProfileFilters($profileFilters)
+    {
+        $this->profileFilters = $profileFilters;
+    }
+
+    /**
+     * @param array $usersFilters
+     */
+    public function setUsersFilters($usersFilters)
+    {
+        $this->usersFilters = $usersFilters;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId()
+    {
+        return $this->id;
+    }
+
+}
