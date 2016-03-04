20672b5fc512633fb681011e77a6862e62319347
Juan Luis Martínez <juanlu@comakai.com>
2016-03-04 17:54:38 +0100

    QS-940: Refactor bootstrap

--- bootstrap-neo4j-testing.php
+++ bootstrap-neo4j-testing.php
@@ -1,28 +1,40 @@
 <?php
 
-$neo4jVersion = '2.2.5';
+define('IS_WINDOWS', strtoupper(substr(PHP_OS, 0, 3)) === 'WIN');
+define('NEO4J_VERSION', '2.2.5');
+define('NEO4J_PATH', 'vendor' . DIRECTORY_SEPARATOR . 'neo4j-community-' . NEO4J_VERSION);
+define('NEO4J_BIN', NEO4J_PATH . DIRECTORY_SEPARATOR . 'bin');
+define('NEO4J_FILE', IS_WINDOWS ? 'windows.zip' : 'unix.tar.gz');
 
-$win = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
-$os = $win ? 'windows.zip' : 'unix.tar.gz';
-
-if (!is_dir("vendor/neo4j-community-{$neo4jVersion}")) {
-    exec("wget http://neo4j.com/artifact.php?name=neo4j-community-{$neo4jVersion}-{$os} -Ovendor/$os");
-    $win ? exec("unzip vendor/$os -d vendor") : exec("tar xvzf vendor/$os -C vendor");
-    exec("rm vendor/$os");
-    exec("sed -i 's/#org.neo4j.server.webserver.address=0.0.0.0/org.neo4j.server.webserver.address=0.0.0.0/g' vendor/neo4j-community-{$neo4jVersion}/conf/neo4j-server.properties");
-    exec("sed -i 's/org.neo4j.server.webserver.port=7474/org.neo4j.server.webserver.port=7475/g' vendor/neo4j-community-{$neo4jVersion}/conf/neo4j-server.properties");
-    exec("sed -i 's/org.neo4j.server.webserver.https.port=7473/org.neo4j.server.webserver.https.port=7476/g' vendor/neo4j-community-{$neo4jVersion}/conf/neo4j-server.properties");
-    exec("mkdir vendor" . DIRECTORY_SEPARATOR . "neo4j-community-{$neo4jVersion}" . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR . "dbms");
-    exec('echo admin:SHA-256,5F5E0B0CEBC8A4FCEEAC6A1E3A778039C7D9912F6C0378CCCBC1F2B1479FCFEA,5DE083E710DD6E7341331DEB11D636D9: > vendor/neo4j-community-' . $neo4jVersion . '/data/dbms/auth');
+if (!is_dir(NEO4J_PATH)) {
+    exec('wget http://neo4j.com/artifact.php?name=neo4j-community-' . NEO4J_VERSION . '-' . NEO4J_FILE . ' -Ovendor/' . NEO4J_FILE);
+    IS_WINDOWS ? exec('unzip vendor/' . NEO4J_FILE . ' -d vendor') : exec('tar xvzf vendor/' . NEO4J_FILE . ' -C vendor');
+    exec('rm vendor/' . NEO4J_FILE);
+    exec("sed -i 's/#org.neo4j.server.webserver.address=0.0.0.0/org.neo4j.server.webserver.address=0.0.0.0/g' " . NEO4J_PATH . '/conf/neo4j-server.properties');
+    exec("sed -i 's/org.neo4j.server.webserver.port=7474/org.neo4j.server.webserver.port=7475/g' " . NEO4J_PATH . '/conf/neo4j-server.properties');
+    exec("sed -i 's/org.neo4j.server.webserver.https.port=7473/org.neo4j.server.webserver.https.port=7476/g' " . NEO4J_PATH . '/conf/neo4j-server.properties');
+    exec('mkdir ' . NEO4J_PATH . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'dbms');
+    exec('echo admin:SHA-256,5F5E0B0CEBC8A4FCEEAC6A1E3A778039C7D9912F6C0378CCCBC1F2B1479FCFEA,5DE083E710DD6E7341331DEB11D636D9: > ' . NEO4J_PATH . '/data/dbms/auth');
 }
 
-$output_ssh = shell_exec("ssh vagrant@local.nekuno.com -o LogLevel=verbose ./../../vagrant/brain/vendor/neo4j-community-{$neo4jVersion}/bin/neo4j restart");
+$output_ssh = shell_exec('ssh vagrant@local.nekuno.com -o LogLevel=verbose ./../../vagrant/brain/' . NEO4J_PATH . '/bin/neo4j restart');
 
 if ($output_ssh === null) {
-    exec("vendor/neo4j-community-{$neo4jVersion}/bin/neo4j restart", $output, $code);
-    if ($code != 0) {
-        trigger_error("Error starting Neo4j");
-        die;
+    if (IS_WINDOWS) {
+        exec(NEO4J_BIN . DIRECTORY_SEPARATOR . 'Neo4jShell.bat -c quit', $out, $code);
+        if ($code !== 0) {
+            exec(NEO4J_BIN . DIRECTORY_SEPARATOR . 'neo4j', $output, $code);
+            if ($code !== 0) {
+                trigger_error('Error starting Neo4j');
+                die;
+            }
+        }
+    } else {
+        exec(NEO4J_BIN . DIRECTORY_SEPARATOR . 'neo4j restart', $output, $code);
+        if ($code !== 0) {
+            trigger_error('Error starting Neo4j');
+            die;
+        }
     }
 }
 
