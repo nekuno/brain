b328a5035856ecbede0288531ebe54fd9a0850fd
Juan Luis Martínez <juanlu@comakai.com>
2016-07-03 11:29:30 +0200

    QS-1052: Remove facebookID properties

--- src/Model/Neo4j/GraphManager.php
+++ src/Model/Neo4j/GraphManager.php
@@ -174,7 +174,7 @@ class GraphManager implements LoggerAwareInterface
     private function copyProperties($id1, $id2)
     {
         //TODO: Improve code placement of unique node properties
-        $unique = array('qnoow_id', 'twitterID', 'googleID', 'facebookID', 'spotifyID', 'usernameCanonical', 'emailCanonical');
+        $unique = array('qnoow_id', 'usernameCanonical', 'emailCanonical');
 
         //get properties
         $qb = $this->createQueryBuilder();
