e44c54f761fdf4f7ed2e42a7d226352731a1505c
Manolo Salsas <manolez@gmail.com>
2016-07-04 17:12:51 +0200

    QS-1052: Remove getUserInformation from TwitterResourceOwner

--- src/Http/OAuth/ResourceOwner/TwitterResourceOwner.php
+++ src/Http/OAuth/ResourceOwner/TwitterResourceOwner.php
@@ -20,18 +20,6 @@ class TwitterResourceOwner extends Oauth1GenericResourceOwner
 
     public $name = TokensModel::TWITTER;
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getUserInformation(array $accessToken, array $extraParameters = array())
-	{
-		if ($this->options['include_email']) {
-			$this->options['infos_url'] = $this->normalizeUrl($this->options['infos_url'], array('include_email' => 'true'));
-		}
-
-		return parent::getUserInformation($accessToken, $extraParameters);
-	}
-
 	public function getAPIRequest($url, array $query = array(), array $token = array())
     {
         $request = parent::getAPIRequest($url, $query, $token);
