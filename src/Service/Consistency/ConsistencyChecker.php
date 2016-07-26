648fabaaef79659bacbadde60233c45eed55e7d8
Roberto Martinez <yawmoght@gmail.com>
2016-07-26 13:31:15 +0200

    QS-986: Add consistency service for nodes and relationships in neo

---
+++ src/Service/Consistency/ConsistencyChecker.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * @author yawmoght <yawmoght@gmail.com>
+ */
+
+namespace Service\Consistency;
+
+
+use Everyman\Neo4j\Label;
+use Everyman\Neo4j\Node;
+use Everyman\Neo4j\PropertyContainer;
+use Everyman\Neo4j\Relationship;
+use Model\Exception\ValidationException;
+use Model\Neo4j\GraphManager;
+use Model\User;
+
+class ConsistencyChecker
+{
+    protected $graphManager;
+    protected $consistency;
+
+    /**
+     * ConsistencyChecker constructor.
+     * @param GraphManager $graphManager
+     * @param array $consistency
+     */
+    public function __construct(GraphManager $graphManager, array $consistency)
+    {
+        $this->graphManager = $graphManager;
+        $this->consistency = $consistency;
+    }
+
+    public function checkUser(User $user) {
+        $qb = $this->graphManager->createQueryBuilder();
+
+        $qb->match('(u:User{qnoow_id: {userId}})')
+            ->setParameter('userId', $user->getId())
+            ->returns('u');
+
+        $result = $qb->getQuery()->getResultSet();
+
+        $userNode = $result->current()->offsetGet('u');
+
+        $this->checkNode($userNode);
+    }
+
+    private function checkNode(Node $node)
+    {
+        /** @var Label[] $labels */
+        $labelNames = $this->getLabelNames($node);
+
+        $rules = $this->consistency;
+        foreach ($rules['nodes'] as $rule) {
+            if (!in_array($rule['label'], $labelNames)) {
+                continue;
+            }
+
+            $nodeRule = new ConsistencyNodeRule($rule);
+
+            $this->checkNodeRelationships($node, $nodeRule->getRelationships());
+
+            $this->checkProperties($node, $nodeRule->getProperties());
+        }
+    }
+
+    /**
+     * @param Node $node
+     * @param $relationshipRules
+     */
+    private function checkNodeRelationships(Node $node, $relationshipRules)
+    {
+        $totalRelationships = $node->getRelationships();
+
+        foreach ($relationshipRules as $relationshipRule) {
+            $rule = new ConsistencyRelationshipRule($relationshipRule);
+
+            /** @var Relationship[] $relationships */
+            $relationships = array_filter($totalRelationships, function ($relationship) use ($rule){
+                /** @var $relationship Relationship */
+                return $relationship->getType() === $rule->getType();
+            });
+
+            $errors = array('relationships' => array());
+
+            if (count($relationships) < $rule->getMinimum()){
+                $errors['relationships'][$rule->getType()] =  sprintf('Amount of relationships %d is less than %d allowed', count($relationships), $rule->getMinimum());
+            }
+
+            if (count($relationships) > $rule->getMaximum()){
+                $errors['relationships'][$rule->getType()] =  sprintf('Amount of relationships %d is more than %d allowed', count($relationships), $rule->getMaximum());
+            }
+
+            foreach ($relationships as $relationship)
+            {
+                $startNode = $relationship->getStartNode();
+                $endNode = $relationship->getEndNode();
+                $otherNode = $startNode->getId() !== $node->getId() ? $startNode : $endNode;
+
+
+                if ($rule->getDirection() == 'incoming' && $endNode->getId() != $node->getId()
+                    || $rule->getDirection() == 'outgoing' && $startNode->getId() != $node->getId()){
+                    $errors['relationships'][] = sprintf('Direction of relationship %d is not correct', $relationship->getId());
+                }
+
+                if (!in_array($rule->getOtherNode(), $this->getLabelNames($otherNode))){
+                    $errors['relationships'][] = sprintf('Label of destination node for relationship %d is not correct', $relationship->getId());
+                }
+
+                $this->checkProperties($relationship, $rule->getProperties());
+            }
+
+            if (!empty($errors['relationships'])){
+                throw new ValidationException($errors, 'Node relationships consistency error for node '.$node->getId());
+            }
+        }
+    }
+
+    private function checkProperties(PropertyContainer $propertyContainer, array $propertyRules)
+    {
+        $properties = $propertyContainer->getProperties();
+        //TODO: min/max of properties, PropertyRule: required (one of X), values of each
+    }
+
+    //checkUsers
+    //checkLinks
+
+    private function getLabelNames(Node $node)
+    {
+        /** @var Label[] $labels */
+        $labels = $node->getLabels();
+        $labelNames = array();
+        foreach ($labels as $label) {
+            $labelNames[] = $label->getName();
+        }
+
+        return $labelNames;
+    }
+}
