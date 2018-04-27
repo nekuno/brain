9346b9ddfccfe1b1e56fd832a1f27a4325711352
msalsas <manolez@gmail.com>
2018-04-27 11:53:32 +0200

    Add required packages and add parameters as env variables (dots removed from names)

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,10 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
---
+++ src/Security/UserProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Security;
+
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
+use Model\User\UserManager;
+use Model\User\User;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface as SecurityUserInterface;
+
+class UserProvider implements UserProviderInterface, OAuthAwareUserProviderInterface
+{
+    /**
+     * @var UserManager
+     */
+    protected $userManager;
+
+    /**
+     * Constructor.
+     *
+     * @param UserManager $userManager
+     */
+    public function __construct(UserManager $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function loadUserByUsername($username)
+    {
+        $user = $this->findUser($username);
+
+        if (!$user) {
+            throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
+        }
+
+        return $user;
+    }
+
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        $resourceId = $response->getUsername();
+        $resourceOwner = $response->getResourceOwner()->getName();
+        try {
+            $user = $this->userManager->findUserByResourceOwner($resourceOwner, $resourceId);
+        } catch (NotFoundHttpException $e) {
+            $e = new UsernameNotFoundException();
+            $e->setUsername($response->getUsername());
+            $e->setToken($response->getOAuthToken());
+            throw $e;
+        }
+
+        return $user;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function refreshUser(SecurityUserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of FOS\UserBundle\Model\User, but got "%s".', get_class($user)));
+        }
+
+        if (!$this->supportsClass(get_class($user))) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of %s, but got "%s".', '\\Manager\\User', get_class($user)));
+        }
+
+        if (null === $reloadedUser = $this->userManager->findUserBy(array('id' => $user->getId()))) {
+            throw new UsernameNotFoundException(sprintf('User with ID "%d" could not be reloaded.', $user->getId()));
+        }
+
+        return $reloadedUser;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supportsClass($class)
+    {
+        $userClass = '\\Manager\\User';
+
+        return $userClass === $class || is_subclass_of($class, $userClass);
+    }
+
+    /**
+     * Finds a user by username.
+     *
+     * This method is meant to be an extension point for child classes.
+     *
+     * @param string $username
+     *
+     * @return UserInterface|null
+     */
+    protected function findUser($username)
+    {
+        return $this->userManager->findUserByUsername($username);
+    }
+}
